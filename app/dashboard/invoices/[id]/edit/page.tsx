496ba791d1d359c019204eb8e694a635d37362af
sfilata <shubaiqiao@126.com>
2024-01-03 17:49:39 +0800

    feat: Complete the invoice CRUD features.

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,35 @@
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
+import Form from '@/app/ui/invoices/edit-form';
+import { updateInvoice } from '@/app/lib/actions';
+
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = params;
+
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Invoices',
+            href: '/dashboard/invoices',
+          },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form
+        invoice={invoice}
+        customers={customers}
+      />
+    </main>
+  );
+}
