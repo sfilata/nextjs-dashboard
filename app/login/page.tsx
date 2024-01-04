da1b6e98ee87c273fba97cd9d87667a054c3883b
sfilata <shubaiqiao@126.com>
2024-01-04 15:01:45 +0800

    feat: Add the user authorization.

---
+++ app/login/page.tsx
@@ -0,0 +1,17 @@
+import AcmeLogo from '../ui/acme-logo';
+import LoginForm from '../ui/login-form';
+
+export default function LoginPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="5 relative mx-auto flex w-full max-w-[400px] flex-col space-y-2 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="md:2-36 w-32 text-white">
+            <AcmeLogo />
+          </div>
+        </div>
+        <LoginForm />
+      </div>
+    </main>
+  );
+}
