0d5af543f661fd7ef7c4bcb377ca827f3d69ec1b
sfilata <shubaiqiao@126.com>
2024-01-04 14:00:02 +0800

    style: Fix the code style.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,12 +4,18 @@ import { lusitana } from '@/app/ui/fonts';
 import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { Suspense } from 'react';
-import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
+import {
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Dashboard</h1>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
         <Card title="Pending" value={totalPendingInvoices} type="pending" />
@@ -23,7 +29,7 @@ export default async function Page() {
           <CardWrapper />
         </Suspense>
       </div>
-      <div className="mt-6 grid gird-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+      <div className="gird-cols-1 mt-6 grid gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           {/* <RevenueChart revenue={revenue} /> */}
           <RevenueChart />
@@ -33,5 +39,5 @@ export default async function Page() {
         </Suspense>
       </div>
     </main>
-  )
+  );
 }
